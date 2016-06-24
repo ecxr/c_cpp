24c188dafc906e77f46f1bed55b2c2ff1848b913
Sky King <skyking@gmail.com>
2016-06-24 06:23:33 -0700

    Dice simulator in cpp

---
+++ week1/dice/dice_cpp/dice_cpp/dice.cpp
@@ -0,0 +1,42 @@
+//
+// The following program computes the probability for dice possibilities
+//
+
+#include <iostream> // drops .h 
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+const int sides = 6;  // replaces #define
+
+inline int r_sides() { return rand() % sides + 1; }
+
+int main(int argc, char* argv[])
+{
+    const int n_dice = 2;
+    int d1, d2;
+    
+    srand(clock());
+
+    cout << "\nEnter number of trials: ";
+    
+    int trials;
+    cin >> trials;
+
+    int *outcomes = new int[n_dice * sides + 1];
+
+    for (int j = 0; j < trials; ++j)
+    {
+        outcomes[(d1 = r_sides()) + (d2 = r_sides())]++;
+    }
+
+    cout << "probability\n";
+
+    for (int j = 2; j < n_dice * sides + 1; ++j)
+    {
+        cout << "j = " << j << " p = "
+            << static_cast<double>(outcomes[j]) / trials
+            << endl;
+    }
+}
